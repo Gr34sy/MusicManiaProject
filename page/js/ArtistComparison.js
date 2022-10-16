54345a6e648124190f9187f2ddcdde069d510b6d
Gr34sy <slupek3@poczta.fm>
2022-10-16 19:09:52 +0200

    added responsiveness to artist cards

--- page/js/ArtistComparison.js
+++ page/js/ArtistComparison.js
@@ -4,9 +4,14 @@ import { Card } from "./Card";
 export function ArtistComparison(){
     return(
         <section className="artist-comparison">
-            <div className="card-box">
-                <Card/>
-                <Card/>
+            <div class="comparison-box">
+                <p className="comparison__title">Similar artist searching</p>
+                
+                <div className="card-box">
+                    <Card/>
+                    <Card/>
+                    <Card/>
+                </div>
             </div>
         </section>
     )
