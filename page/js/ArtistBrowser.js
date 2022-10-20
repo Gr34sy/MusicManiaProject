c224d80d82290be8e20b2c7de321239bed60ecd1
Gr34sy <slupek3@poczta.fm>
2022-10-20 02:12:18 +0200

    added details logic in artist browser section

--- page/js/ArtistBrowser.js
+++ page/js/ArtistBrowser.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState } from "react";
 
 export function ArtistBrowser(){
-    const [filterValue, setFilterValue] = useState('metal');
+    const [browserFilterValue, setBrowserFilterValue] = useState('metal');
     const [browserFilter,setBrowserFilter] = useState([]);
-    const [browserDetails, setBrowserDetails] = useState([]);
+
+    const [detailsFilterValue, setDetailsFilterValue] = useState()
+    const [artistDetails, setArtistDetails] = useState({name: "Click on artist to get details!"});
+    const [artistTopTracks, setArtistTopTracks] = useState();
     
     const rootAPI = 'http://ws.audioscrobbler.com/2.0';
     const keyAPI = '4d2a662e3ae0be5759a731d889e084d1';
     
 
     useEffect(() => {
-        fetch(`${rootAPI}/?method=tag.gettopartists&tag=${filterValue}&api_key=${keyAPI}&format=json`)
+        fetch(`${rootAPI}/?method=tag.gettopartists&tag=${browserFilterValue}&api_key=${keyAPI}&format=json`)
         .then(response => {
             if(response.ok){
                 return response
@@ -20,11 +23,45 @@ export function ArtistBrowser(){
         .then(response => response.json())
         .then ( data => setBrowserFilter(data.topartists.artist))
         .catch(err => console.error(err))
-    }, [filterValue]);
+    }, [browserFilterValue]);
+
+
+    //useEffect tylko po update detailsFilterValue
+    useEffect(() => {
+        if(detailsFilterValue){
+            //setting artistdetails state
+            fetch(`${rootAPI}/?method=artist.getinfo&artist=${detailsFilterValue}&api_key=${keyAPI}&format=json`)
+            .then(response => {
+                if(response.ok){
+                    return response
+                }
+                throw Error(response.status);
+            })
+            .then(response => response.json())
+            .then ( data => setArtistDetails(data.artist))
+            .catch(err => console.error(err));
+
+            //setting toptracks state
+            fetch(`${rootAPI}/?method=artist.getinfo&artist=${detailsFilterValue}&api_key=${keyAPI}&format=json`)
+            .then(response => {
+                if(response.ok){
+                    return response
+                }
+                throw Error(response.status);
+            })
+            .then(response => response.json())
+            .then ( data => setArtistTopTracks(data))
+            .catch(err => console.error(err))
+        }
+    }, [detailsFilterValue]);
 
     function handleFilterChange(e){
         e.preventDefault();
-        setFilterValue(e.target.value);
+        setBrowserFilterValue(e.target.value);
+    }
+
+    function handleLineClick(e){
+        setDetailsFilterValue(e.target.getAttribute('data-name'));
     }
 
     return(
@@ -34,7 +71,7 @@ export function ArtistBrowser(){
                     Browse all <span>music artists</span> around the globe!
                 </h2>
 
-                <select className="browser__filter" value={filterValue} onChange={handleFilterChange}>
+                <select className="browser__filter" value={browserFilterValue} onChange={handleFilterChange}>
                     <option value="metal">Metal</option>
                     <option value="nu metal">Nu Metal</option>
                     <option value="rock">Rock</option>
@@ -44,10 +81,15 @@ export function ArtistBrowser(){
                         {browserFilter.map(
                             (artist, id)=>(
                             <li key={id}>
-                                {artist.name} 
+                                <p data-name={artist.name} onClick={handleLineClick}>
+                                    {artist.name}
+                                </p> 
+
                                 <a href={artist.url} className="lastFM-artist-link lastFM-artist-link--screen" target="_blank">
                                     Check out this artist on last.fm!
                                 </a>
+
+                                {/* mobile link */}
                                 <a href={artist.url} className="lastFM-artist-link lastFM-artist-link--mobile" target="_blank">
                                     Artist profile
                                 </a>
@@ -58,11 +100,18 @@ export function ArtistBrowser(){
 
 
                 <div className="details-box">
-                    <p>Click artist to see details</p>
+                    <p>{artistDetails.name}</p>
                 </div>
                 
             </div>
         </section>
     )
 }
+
+// { artistDetails === true ?
+//     <div>
+//         <p>{artistDetails.name}</p>
+//     </div>
+// : <p> Click on artist to get details!</p>
+// }
 
